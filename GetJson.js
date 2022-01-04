4a7656de798d72e56e32ea0901659721a7b4d3bc
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-01-03 22:14:22 -0800

    it works sorta

--- GetJson.js
+++ GetJson.js
@@ -3,6 +3,13 @@ $.getJSON(searchName, function(data) {
     // JSON result in data
     
     //set name field
-    document.getElementById("name").textContent = "Name: " + data.name;
+    document.getElementById("site").textContent = "url: " + data.site;
+
+    //set url field
+    if(data.name != null){
+        document.getElementById("name").textContent = "Name: " + data.name;
+    }else{
+        document.getElementById("name").textContent = "Name: " + data.site;
+    }
 });
 }
