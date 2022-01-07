cc898630cdae45145356ea5367b1822774aa853f
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-01-07 01:24:57 -0800

    tired

---
+++ DarkViewer/scripts/resetPage.js
@@ -0,0 +1,8 @@
+function resetPage(){
+    document.getElementById("site").textContent = ""
+    document.getElementById("name").textContent = ""
+    document.getElementById('pfp').src= "https://i.imgur.com/ZVA8eHl.png";
+    document.getElementById("pronouns").textContent = "";
+    document.getElementById("people").textContent = "No friends :(";
+    document.getElementById("Contacts").textContent = "friends";
+}
